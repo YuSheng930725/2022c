68e2441a272a1804ce4df845617d4ff90983382d
YuSheng930725 <114201619+YuSheng930725@users.noreply.github.com>
2022-09-30 11:57:09 +0800

    week04

---
+++ week04-5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    for( int i=0; i<12; i++ ) {
+        printf("印出%d\n",i);
+    }
+    for(int i=1; i<=12; i++) {
+        printf("人類的版本，印出:%d\n",i);
+    }
+}
+///K&R
